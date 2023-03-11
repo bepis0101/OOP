6cd7b1d9e95b99cd70cfc6128e7c91e55b021135
bepis0101 <borysadamiak@interia.eu>
2023-03-11 11:56:45 +0100

    done projects

---
+++ stream.cs
@@ -0,0 +1,12 @@
+/*
+Borys Adamiak
+Lista 2 Programowanie Obiektowe
+kompilator mcs stream.cs a nastepnie ./stream.exe
+zadanie 1
+*/
+
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
